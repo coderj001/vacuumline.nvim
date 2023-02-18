e32efae373143a5f48855ea7375991a3a6aac066
Raju Ghorai <amirajubolchi001@gmail.com>
2023-02-18 22:15:37 +0530

    update colorscheme

--- lua/vacuumline/theme/tokoynight_storm.lua
+++ lua/vacuumline/theme/tokoynight_storm.lua
@@ -1,23 +1,24 @@
 local palette = {
-  black         = '#1a1b26',
-  red           = '#e06c75',
-  green         = '#98c379',
-  yellow        = '#d19a66',
-  blue          = '#61afef',
-  purple        = '#c678dd',
-  cyan          = '#56b6c2',
-  white         = '#bbc2cf',
-  bright_black  = '#5c6370',
-  bright_red    = '#e06c75',
-  bright_green  = '#98c379',
-  bright_yellow = '#d19a66',
-  bright_blue   = '#61afef',
-  bright_purple = '#c678dd',
-  bright_cyan   = '#56b6c2',
-  bright_white  = '#ffffff',
-  background    = '#24283b',
-  foreground    = '#bbc2cf'
+  black         = '#3b4252',
+  red           = '#bf616a',
+  green         = '#a3be8c',
+  yellow        = '#ebcb8b',
+  blue          = '#81a1c1',
+  purple        = '#b48ead',
+  cyan          = '#88c0d0',
+  white         = '#e5e9f0',
+  bright_black  = '#4c566a',
+  bright_red    = '#bf616a',
+  bright_green  = '#a3be8c',
+  bright_yellow = '#ebcb8b',
+  bright_blue   = '#81a1c1',
+  bright_purple = '#b48ead',
+  bright_cyan   = '#8fbcbb',
+  bright_white  = '#eceff4',
+  background    = '#2e3440',
+  foreground    = '#1a1b26'
 }
+
 
 local theme = {
   line             = {foreground = palette.white, background = palette.black},
